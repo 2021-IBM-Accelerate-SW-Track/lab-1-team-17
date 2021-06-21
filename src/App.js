6f875c1fb4457b5b78f517dc60ef8edc10c851c3
IamBlackMagic <jachike@gmail.com>
2021-06-21 16:38:46 -0500

    input form
    created a slight input form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./component/header"
+//importing components
+import Form from "./component/inputFilter/inputBox";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Form/>
     </div>
   );
 }
